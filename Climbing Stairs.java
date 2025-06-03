62adb48ae7c7d26c52cc513bd640b264f5b48c1d
Mukesh Kumar <155368282+Mukesh915306@users.noreply.github.com>
2025-06-04 01:00:06 +0530

    Create Climbing Stairs.java

---
+++ Climbing Stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    private Map<Integer, Integer> memo = new HashMap<>();
+    public int climbStairs(int n) {
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        int res = climbStairs(n-1) + climbStairs(n-2);
+        memo.put(n, res);
+
+        return res;
+    }
+
+}
