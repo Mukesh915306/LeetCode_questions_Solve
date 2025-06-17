b3b03b3a49fe15bcdb1b6526b33bb441616367f4
Mukesh Kumar <155368282+Mukesh915306@users.noreply.github.com>
2025-06-17 22:31:22 +0530

    Create Length of array.java

---
+++ Length of array.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        int[] arr = {2,4,3,6,7,9,5,4,6};
+        System.out.println("Length of array " + arr.length);
+        
+    }
+}
